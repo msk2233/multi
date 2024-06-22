8c336f488747b7140ea0e25e30bbbee7a94bbef9
shruti.mokariya <shruti.mokariya.2024@gmail.com>
2024-06-22 13:02:51 +0530

    added

--- client/src/actions/action.ts
+++
@@ -1,9 +0,0 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axiosInstance from '../api/axiosInstance'
-
-export const Fetch = createAsyncThunk("Fetch", async () => {
-    const response = await axiosInstance.get("/fetchdetail");
-    const resData = response.data;
-    return resData;
-  });
-
